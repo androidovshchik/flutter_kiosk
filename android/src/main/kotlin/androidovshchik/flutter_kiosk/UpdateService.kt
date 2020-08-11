bd4e616bfa5394c184b497d5b5341ba83515b02c
vlad <vladkalyuzhnyu@gmail.com>
2020-08-11 18:36:57 +0300

    changes

--- android/src/main/kotlin/androidovshchik/flutter_kiosk/UpdateService.kt
+++ android/src/main/kotlin/androidovshchik/flutter_kiosk/UpdateService.kt
@@ -2,9 +2,10 @@ package androidovshchik.flutter_kiosk
 
 import android.app.IntentService
 import android.content.Intent
-import android.content.pm.PackageInstaller
+import android.content.pm.PackageInstaller.SessionParams
 import android.os.ResultReceiver
 import androidovshchik.flutter_kiosk.extension.isConnected
+import androidovshchik.flutter_kiosk.extension.isDeviceOwner
 import androidovshchik.flutter_kiosk.extension.pendingReceiverFor
 import org.jetbrains.anko.connectivityManager
 import java.net.HttpURLConnection
@@ -13,7 +14,7 @@ import java.net.URL
 class UpdateService : IntentService("UpdateService") {
 
     override fun onHandleIntent(intent: Intent?) {
-        if (!connectivityManager.isConnected) {
+        if (!isDeviceOwner || !connectivityManager.isConnected) {
             sendResult(intent, -1)
             return
         }
@@ -23,33 +24,30 @@ class UpdateService : IntentService("UpdateService") {
             val connection = URL(url).openConnection() as HttpURLConnection
             connection.requestMethod = "HEAD"
             connection.connect()
-            if (connection.contentType != "application") {
-                Timber.e("Invalid mime type of apk ${body.contentType()}")
-                sendResult(intent ?: return, -1)
-                return
+            if (connection.contentType?.startsWith("application") != true) {
+                throw Exception("Invalid mime type of apk \"${connection.contentType}\"")
             }
-            if (adminManager.isDeviceOwner) {
-                val params = PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL)
-                params.setAppPackageName(packageName)
-                val packageInstaller = packageManager.packageInstaller
-                val sessionId = packageInstaller.createSession(params)
-                packageInstaller.openSession(sessionId).use {
-                    it.openWrite(packageName, 0, -1).use { output ->
-                        connection.inputStream.copyTo(output)
-                        it.fsync(output)
+            val packageInstaller = packageManager.packageInstaller
+            val params = SessionParams(SessionParams.MODE_FULL_INSTALL)
+            params.setAppPackageName(packageName)
+            val sessionId = packageInstaller.createSession(params)
+            packageInstaller.openSession(sessionId).use { session ->
+                session.openWrite(packageName, 0, -1).use { output ->
+                    connection.inputStream.use { input ->
+                        input.copyTo(output)
                     }
-                    it.commit(
-                        pendingReceiverFor(Intent.ACTION_PACKAGE_ADDED, sessionId)
-                            .intentSender
-                    )
+                    session.fsync(output)
                 }
-                sendResult(intent, 0)
-                return
+                session.commit(
+                    pendingReceiverFor(Intent.ACTION_PACKAGE_ADDED, sessionId)
+                        .intentSender
+                )
             }
+            sendResult(intent, 0)
         } catch (e: Throwable) {
             e.printStackTrace()
+            sendResult(intent, -1)
         }
-        sendResult(intent, -1)
     }
 
     private fun sendResult(intent: Intent?, resultCode: Int = 0) {
